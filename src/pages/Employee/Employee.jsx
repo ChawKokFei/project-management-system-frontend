886136341f154503797997e30f603513af461300
Chaw Kok Fei <80729400+ChawKokFei@users.noreply.github.com>
2023-05-09 17:24:52 +0800

    Add edit employee

--- src/pages/Employee/Employee.jsx
+++ src/pages/Employee/Employee.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import useHttp from "../../hooks/use-http";
 
 const Employee = (props) => {
+  const BASE_URL_FOR_EMPLOYEE = "http://localhost:8099/api/v1/employees";
   const [employees, setEmployees] = useState(null);
   const [errorGet, sendGetRequest] = useHttp();
   const [errorDelete, sendDeleteRequest] = useHttp();
   const [errorPost, sendPostRequest] = useHttp();
+  const [errorPut, sendPutRequest] = useHttp();
   const [selectedEmployees, setSelectedEmployees] = useState([]);
   const [newEmployee, setNewEmployee] = useState({
     firstName: "",
@@ -13,12 +15,32 @@ const Employee = (props) => {
     employeeNo: "",
     dob: "",
   });
+  const [editEmployeeId, setEditEmployeeId] = useState(null);
+
+  const resetNewEmployee = () => {
+    setNewEmployee({
+      firstName: "",
+      lastName: "",
+      employeeNo: "",
+      dob: "",
+    });
+  };
 
   const getAllEmployees = (employees) => {
     // Sort by id in ascesding order
     employees.sort((a, b) => a.id - b.id);
 
     setEmployees(employees);
+  };
+
+  const getEmployee = (employee) => {
+    setNewEmployee({
+      firstName: employee.firstName,
+      lastName: employee.lastName,
+      employeeNo: employee.employeeNo,
+      dob: employee.dob,
+    });
+    setEditEmployeeId(employee.id);
   };
 
   const handleCheckboxChange = (event, employee) => {
@@ -42,88 +64,120 @@ const Employee = (props) => {
     });
   };
 
+  const handleEdit = () => {
+    if (selectedEmployees.length === 1) {
+      sendGetRequest(
+        BASE_URL_FOR_EMPLOYEE.concat("/").concat(selectedEmployees[0]),
+        "get",
+        null,
+        getEmployee
+      );
+    }
+  };
+
   const handleDelete = () => {
     const payload = { ids: selectedEmployees };
 
     sendDeleteRequest(
-      "http://localhost:8099/api/v1/employees",
+      BASE_URL_FOR_EMPLOYEE,
       "delete",
       payload,
       getAllEmployees
     );
   };
 
-  const handleEmployeeSubmit = (event) => {
+  const handleEmployeeSubmit = (event, method) => {
     event.preventDefault();
 
-    sendPostRequest(
-      "http://localhost:8099/api/v1/employees",
-      "post",
-      newEmployee,
-      getAllEmployees
-    );
+    if (method === "post") {
+      sendPostRequest(
+        BASE_URL_FOR_EMPLOYEE,
+        "post",
+        newEmployee,
+        getAllEmployees
+      );
+    } else if (method === "put") {
+      sendPutRequest(
+        BASE_URL_FOR_EMPLOYEE.concat("/").concat(editEmployeeId),
+        "put",
+        newEmployee,
+        getAllEmployees
+      );
+      setEditEmployeeId(null);
+    }
+
+    resetNewEmployee();
   };
 
   useEffect(() => {
-    sendGetRequest(
-      "http://localhost:8099/api/v1/employees",
-      "get",
-      null,
-      getAllEmployees
-    );
+    sendGetRequest(BASE_URL_FOR_EMPLOYEE, "get", null, getAllEmployees);
   }, []);
 
   return (
     <div className="vw-100 vh-100">
       <div className="col-sm-10 mx-auto mt-4">
-        <form>
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th scope="col">Select</th>
-                <th scope="col">#</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">Employee No</th>
-                <th scope="col">DOB</th>
-              </tr>
-            </thead>
-            <tbody>
-              {employees !== null &&
-                employees.map((employee, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          checked={selectedEmployees.includes(employee.id)}
-                          onChange={(event) =>
-                            handleCheckboxChange(event, employee)
-                          }
-                        />
-                      </td>
-                      <th scope="row">{employee.id}</th>
-                      <td>{employee.firstName}</td>
-                      <td>{employee.lastName}</td>
-                      <td>{employee.employeeNo}</td>
-                      <td>{employee.dob}</td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
-          <button
-            type="button"
-            className="btn btn-danger"
-            onClick={handleDelete}
-          >
-            Delete
-          </button>
-        </form>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Select</th>
+              <th scope="col">#</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Employee No</th>
+              <th scope="col">DOB</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees !== null &&
+              employees.map((employee, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        checked={selectedEmployees.includes(employee.id)}
+                        onChange={(event) =>
+                          handleCheckboxChange(event, employee)
+                        }
+                      />
+                    </td>
+                    <th scope="row">{employee.id}</th>
+                    <td>{employee.firstName}</td>
+                    <td>{employee.lastName}</td>
+                    <td>{employee.employeeNo}</td>
+                    <td>{employee.dob}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+        <button
+          type="button"
+          className="btn btn-success me-3"
+          onClick={handleEdit}
+        >
+          Edit
+        </button>
+        <button type="button" className="btn btn-danger" onClick={handleDelete}>
+          Delete
+        </button>
 
         <h3 className="mt-5">Add new employee</h3>
-        <form onSubmit={handleEmployeeSubmit}>
+        <form
+          onSubmit={(event) => {
+            let method = null;
+            console.log(editEmployeeId);
+
+            if (editEmployeeId !== null) {
+              method = "put";
+            } else {
+              method = "post";
+            }
+
+            handleEmployeeSubmit(event, method);
+          }}
+        >
           <div className="mb-3">
             <label className="form-label">First Name</label>
             <input
